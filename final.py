061675e7d7f31b57112d6acb92edc4ea3cd2f246
profdrskipper <prof.dr.skipper@protonmail.com>
2021-06-20 12:30:19 +0000

    small scale program final

--- final.py
+++ final.py
@@ -31,6 +31,7 @@ for site in sites_index.keys():
     Q[(site, site)] += cost
 
 
+
 #   Constraint 1 = min ave distance to existing chargers in neighborhood
 for site, val in sites_index.items():
     #   site = (zone, sector)
@@ -38,3 +39,35 @@ for site, val in sites_index.items():
 
     #   sum of weighted edges
     sum_of_edges  = graph_sc.degree(val[0], weight='weight')
+    #   number of edges
+    num_edges = graph_sc.degree(val[0])
+    #   average = sum / number
+    if num_edges > 0:
+        ave_dist = sum_of_edges / num_edges
+    else:   #   node has no neighbors that are existing chargers, set a large value
+        ave_dist = 100
+    Q[(site, site)] += ave_dist
+
+    
+
+
+#   Constraint 2 = min ave distance to other new zones in neighborhood
+for site1, val in sites_index.items():
+    #   site = (zone, sector)
+    #   val = (index, lat, long)
+
+    for val2_index in graph_ss.neighbors(val[0]):
+        #   quadratic penalty based on distance to neighbors
+        Q[(site1, rev_index[val2_index])] += dist_mat_ss[val[0], val2_index]
+
+
+#   Constraint 3 => number of chargers per zone = 1
+
+chargers_per_zone = 1
+gamme_3 = 150
+for zone in zones.keys():
+    for i in range(zones[zone]):
+        Q[(zone, i), (zone, j)] += (-2 * )
+
+
+        ##  MORE WORK TO DO - INCOMPLETE -> n_final version
